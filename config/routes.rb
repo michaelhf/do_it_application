4a1d640116fb6704d3388bfa5c0c72b9358d81f9
michaelhf <michaelhf@gmail.com>
2015-06-04 19:30:00 -0500

    fixed login

--- config/routes.rb
+++ config/routes.rb
@@ -86,10 +86,11 @@ Rails.application.routes.draw do
   get "/delete_item/:id", :controller => "items", :action => "destroy"
   #------------------------------
 
-  devise_for :users
+devise_for :users, :controllers => { registrations: 'registrations' }
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-       root to: "rankings#index"
+  root to: "rankings#index"
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
